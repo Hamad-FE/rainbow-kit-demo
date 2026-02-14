6e4d166f1a454272a4ccee050e680456d6b0e887
Hamad-FE <hammad.ali@chaingpt.tech>
2026-02-14 22:39:22 +0500

    widget rainbowkit demo

---
+++ app/provider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+
+import { WagmiProvider } from "wagmi";
+import { mainnet, sepolia, base} from "wagmi/chains";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "Rainbow Demo",
+  projectId: "df55a54a29fb7c20e7579edecbcdcd40", // replace this
+  chains: [mainnet, sepolia, base],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
